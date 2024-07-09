fd4b0cf12c6bc9cbc38a9abd0cb1b5da6b22eb5d
ushasri <badaushasri4@gmail.com>
2024-07-09 16:19:46 +0530

    coding with code.java

---
+++ code.java
@@ -0,0 +1,6 @@
+class java
+{
+    public static void main(String args[])
+System.out.println("name : " +ushasri);
+System.out.println("age:" System.out.println("ushasri");+21);
+}
---
+++ code.py
@@ -0,0 +1,1 @@
+print("ushasri bada")
